4b6245689ad0e899678205b18dc5dc1758c5cc4c
Christian Watts <civrev@gmail.com>
2017-04-05 19:05:58 -0400

    SQLite fixes
    I finished up some work on the Database. You can now search for a
    specific berry, and that berry's stats by using its name. I'll add to
    the db a key to make sure that berry name remains unique, but later.
    right now, just don't make 2 berries with the same name.

--- BramberiFarms/app/src/main/java/mobileapps/bramberifarms/TestActivity.java
+++ BramberiFarms/app/src/main/java/mobileapps/bramberifarms/TestActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 import java.text.DecimalFormat;
 
 import static mobileapps.bramberifarms.BerryTools.pullBerry;
+import static mobileapps.bramberifarms.BerryTools.pullStats;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -22,12 +23,23 @@ public class TestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test);
 
         final Button backBtn = (Button) findViewById(R.id.backBtn);
+        //for berry
         final EditText nF = (EditText) findViewById(R.id.nameField);
         final EditText sF = (EditText) findViewById(R.id.seasonField);
         final EditText idF = (EditText) findViewById(R.id.idField);
         final Button entBtn = (Button) findViewById(R.id.entBtn);
         final Button pullBtn = (Button) findViewById(R.id.pullBtn);
         final TextView outL = (TextView) findViewById(R.id.outLabel);
+        //for stats
+        final EditText bidStatF = (EditText) findViewById(R.id.bidStatF);
+        final EditText yearStatF = (EditText) findViewById(R.id.yearStatF);
+        final EditText plantsStatF = (EditText) findViewById(R.id.plantsStatF);
+        final EditText yieldStatF = (EditText) findViewById(R.id.yieldStatF);
+        final EditText marketStatF = (EditText) findViewById(R.id.marketStatF);
+        final EditText priceStatF = (EditText) findViewById(R.id.priceStatF);
+        final Button statInBtn = (Button) findViewById(R.id.statInBtn);
+        final Button statOutBtn = (Button) findViewById(R.id.statOutBtn);
+        final TextView statOut = (TextView) findViewById(R.id.statOut);
 
 
         entBtn.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +62,33 @@ public class TestActivity extends AppCompatActivity {
                     outL.setText(berry.toString());
                 } else {
                     outL.setText("No berry found");
+                }
+            }
+        });
+
+        statInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String brryId = bidStatF.getText().toString();
+                int year = Integer.parseInt(yearStatF.getText().toString());
+                int numplants = Integer.parseInt(plantsStatF.getText().toString());
+                double yieldpp = Double.parseDouble(yieldStatF.getText().toString());
+                double marketyield = Double.parseDouble(marketStatF.getText().toString());
+                double pricepp = Double.parseDouble(priceStatF.getText().toString());
+                YieldStat ystat = new YieldStat(brryId, year, numplants, yieldpp, marketyield, pricepp);
+                BerryTools.inputStats(ystat);
+            }
+        });
+
+        statOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nF.getText().toString();
+                YieldStat ys = pullStats(name);
+                if (ys!=null) {
+                    statOut.setText(ys.toString());
+                } else {
+                    statOut.setText("No stat for that berry");
                 }
             }
         });
